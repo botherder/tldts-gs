bc85375af774aa53e76447032fd66ff83faa4128
Nex <nex@nex.sx>
2019-07-15 21:32:42 +0200

    I don't know

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,14 @@
 const GasPlugin = require('gas-webpack-plugin');
-const BabelPlugin = require('babel-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    mode: 'none',
+    context: __dirname,
     entry: './src/index.js',
     output: {
         path: __dirname,
-        filename: 'dist/index.js'
+        filename: 'dist/Code.gs'
     },
     plugins: [
-        new BabelPlugin({ presets: ["gas"] }),        
         new GasPlugin(),
         new CopyPlugin([{from: 'appsscript.json', to: 'dist/appsscript.json'}])
     ]
